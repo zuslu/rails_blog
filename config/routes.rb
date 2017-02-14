8af23bc6b521f56ad098850224b1a5501baa2cc4
zeynepUslu <zuslu1@binghamton.edu>
2017-02-13 22:23:51 -0500

    Jumpstart lab11 form-based workflow completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'articles#index'
   resources :articles 
   #this lines tells Rails that we have a resource named 
   #articles when a request like http://localhost:3000/articles/
   #is made, the router will understand you are looking for  
   #a listing of articles
+
 end
